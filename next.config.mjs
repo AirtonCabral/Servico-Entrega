e287b3e94c35cfbd80641f99b2c9a66221ed4e57
Airton Cabral <designnoturno@gmail.com>
2026-08-17 02:05:43 -0300

    Configure Next.js for export and custom base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/Servico-Entrega',
+  assetPrefix: '/Servico-Entrega/',
+  images: {
+    unoptimized: true,
+  },
   experimental: {
     serverComponentsExternalPackages: ["tesseract.js"],
   },
