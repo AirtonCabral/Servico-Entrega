7b213e93be19e5ab6fdfd0f68532e614e93cc213
Airton Cabral <designnoturno@gmail.com>
2026-08-17 02:24:25 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/Servico-Entrega',
-  assetPrefix: '/Servico-Entrega/',
-  images: {
-    unoptimized: true,
-  },
   experimental: {
     serverComponentsExternalPackages: ["tesseract.js"],
   },
